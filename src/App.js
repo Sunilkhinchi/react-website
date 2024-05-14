93aa6b30bf6c2cdf6d77d7f17eea724f1506a3e5
sunil khinchi <sunil.khinchi@aurigait.com>
2024-05-14 11:16:33 +0530

    push the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
 
-function App() {
+import './App.scss';
+
+const App = () => {
+
+  // const theme = () => {
+  //   colors : {
+      
+  //   }
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+    // s
+  ); 
+};
 
 export default App;
